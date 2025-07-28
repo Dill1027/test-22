41def28bfb19d254b91729165220765c29ed9d11
Prabhath Dill <145959457+Dill1027@users.noreply.github.com>
2025-07-28 22:31:45 +0530

    Setup basic React frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to MERN Frontend!</h1>
+      <p>This is a basic React setup.</p>
+    </div>
+  );
+}
+
+export default App;
